892c5763f99c26682fd4851765eac83ada79ea27
Da-Jin Chu <dajinchu@gmail.com>
2016-03-16 06:09:02 -0400

    added comments

--- core/src/train/chu/chu/Block.java
+++ core/src/train/chu/chu/Block.java
@@ -18,35 +18,41 @@ public class Block extends HorizontalGroup {
     private Rectangle centerRect = new Rectangle();
 
     public Block(final DragAndDrop dad){
-        dad.addSource(new DragAndDrop.Source(this) {
-            public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
+        //DragAndDrop has Source, Payload, and Target
+        //Source is what we drag from.
+        //Payload is the thing getting dragged.
+        //Targets are things that payloads can be dropped onto. They listen for payloads interacting with them.
+        //When blocks are dragged, they create a lookalike for the Payload
 
+        //Add a Source to the drag and drop manager, representing this block.
+        dad.addSource(new DragAndDrop.Source(this) { //Give source this Block in constructor as the SourceActor
+            public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
+                //This block is being dragged. We have to supply a Payload to get dragged with the mouse.
                 DragAndDrop.Payload payload = new DragAndDrop.Payload();
                 payload.setObject("Some payload!");
 
+                //Set this block invisible. It should appear to the user that they are dragging the
+                // block itself, not a look-alike. This block should disappear, and leave only the payload visible
                 Block.this.setVisible(false);
 
+                //Duplicate this block, and set that Actor as the Payload dragActor.
+                // Duplication does NOT create a Block, merely a WidgetGroup lookalike. Making it a block
+                // might be problematic, as then the payload Block would have all the functions of a placed Block.
+                // It just needs to look the same.
                 WidgetGroup dragActor = getDuplicateForDragging();
-                dragActor.invalidate();
-                dragActor.layout();
                 payload.setDragActor(dragActor);
+                //setDragActorPosition is to offset the dragActor from the pointer location
+                // without this part, the pointer is always dragging the actor by its left edge
+                // instead, this offsets to be held by where it was picked up
                 dad.setDragActorPosition(-x,(dragActor.getPrefHeight()/2-y));
 
-
-                System.out.println(getChildren().get(0).getName()+": "+-(dragActor.getPrefWidth()/2)+", "+dragActor.getPrefHeight()/2+"  click "+x+", "+y);
-                /*Label validLabel = new Label("valid!", skin);
-                    validLabel.setColor(0, 1, 0, 1);
-                    payload.setValidDragActor(validLabel);
-
-                    Label invalidLabel = new Label("invalid!", skin);
-                    invalidLabel.setColor(1, 0, 0, 1);
-                    payload.setInvalidDragActor(invalidLabel);
-*/
                 return payload;
             }
 
             @Override
             public void dragStop(InputEvent event, float x, float y, int pointer, DragAndDrop.Payload payload, DragAndDrop.Target target) {
+                //Drag stopped. Wherever block ended up, make it visible again. Payload is automatically
+                // destroyed by DragAndDrop
                 Block.this.setVisible(true);
             }
         });
@@ -54,16 +60,18 @@ public class Block extends HorizontalGroup {
             public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
                 //Something is being dragged over this target
                 if (source.getActor() == Block.this) {
-                    System.out.println("Same actor");
+                    //This probably won't happen. But to be sure, this prevents dragging over itself
                     return false;
                 }
 
                 if (centerRect.contains(x, y)) {
-                    System.out.println("contained");
-                    System.out.println(getParent().swapActor(getActor(), source.getActor()));
+                    //The centerRect has been entered, go swap with payload's source.
+                    // To the user this looks this block is getting out of the way of what's being dragged
+                    // It's a little different in code because the payload is just sort of 'representing'
+                    // the source Block.
+                    getParent().swapActor(getActor(), source.getActor());//TODO this probably really shouldn't be a swap: it looks weird when you go around other blocks to place one far away. shouldn't be swap, it should remove source, and insert it here, but it'll have to decide which side of this block to put it on
                     ((WidgetGroup) getParent()).invalidate();
                 }
-                System.out.println("drag");
                 getActor().setColor(Color.GREEN);
                 return true;
             }
@@ -79,14 +87,21 @@ public class Block extends HorizontalGroup {
     }
 
     private WidgetGroup getDuplicateForDragging(){
+        //Create a lookalike of this block, for Payloads
         WidgetGroup g = new HorizontalGroup();
+        //Iterate through children and add them to the clone group
         for(Actor a : getChildren()) {
+            //TODO extract and improve the duping process into some kind of CloneUtils that would clone Actors
             if (a.getClass() == Block.class) {
+                //If the child is a block class, ask that nested block to duplicate too
                 g.addActor(((Block) a).getDuplicateForDragging());
             } else {
+                //Otherwise, it's a label, so cast it and get access to text and style
                 Label aLabel = (Label) a;
+                //Create a new Label instance, and give it the same properties as the old one
                 Label dupe = new Label(aLabel.getText(), aLabel.getStyle());
                 dupe.setColor(0, 0, 0, 1);
+                //And add it to the clone group, of course
                 g.addActor(dupe);
             }
         }
@@ -96,6 +111,9 @@ public class Block extends HorizontalGroup {
     @Override
     public void layout() {
         super.layout();
+        //When this block's layout has to be recalculated (see super.layout()), we can also recalculate the centerRect.
+        //This can't be done just once on creation, because the children might not have been added,
+        // and the block's children could change. This is the best place to calculate centerRect.
         centerRect.set(
                 getWidth() * .3f, 0,
                 getWidth() * .4f, getHeight());
--- core/src/train/chu/chu/Main.java
+++ core/src/train/chu/chu/Main.java
@@ -10,13 +10,11 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
-import com.badlogic.gdx.utils.SnapshotArray;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -34,31 +32,41 @@ public class Main extends ApplicationAdapter {
 
     @Override
 	public void create () {
+        //Instantiate batch+shapes for drawing, camera for transforming, and img for temporary use
+        //note that Stage has its own camera. this one is different
 		batch = new SpriteBatch();
         shapes = new ShapeRenderer();
 		img = new Texture("badlogic.jpg");
         camera = new OrthographicCamera();
 
-        //Generate font
+        //Generate bitmap font from TrueType Font
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Roboto-Light.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 72;
-        BitmapFont roboto = generator.generateFont(parameter); // font size 12 pixels
+        BitmapFont roboto = generator.generateFont(parameter);
         generator.dispose();
 
+        //Instantiate Stage for scene2d management
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
+
+        //Load skin with images and styles for use in scene2d ui elements
         skin = new Skin();
         skin.add("default", new Label.LabelStyle(roboto, Color.WHITE));
         skin.add("badlogic", new Texture("badlogic.jpg"));
         skin.add("tmp", new Texture("tmp.png"));
 
+        //row is the outermost ui element for the sandbox, it holds all the blocks
+        //Really, it should be a block too, but all blocks are drag-and-drop-able, and can't be nested
+        // so that wouldn't work.
         row = new HorizontalGroup();
         row.setBounds(0,0,300,300);
         stage.addActor(row);
 
+        //Instantiate the DragAndDrop manager
         dragAndDrop = new DragAndDrop();
 
+        //Instantiate labels and put them each in a block. Add each block to row
         Label first = new Label("*",skin);
         first.setColor(Color.BLACK);
         Block block = new Block(dragAndDrop);
@@ -77,35 +85,45 @@ public class Main extends ApplicationAdapter {
         block.addActor(num);
 
         row.addActor(block);
-
-        SnapshotArray<Actor> children = row.getChildren();
-        for(final Actor actor : children) {
-
-        }
 	}
 
     @Override
     public void resize(int width, int height) {
+        //Handle resize. This is still important on static windows (eg. Android) because it is
+        // called once in the beginning of the app lifecycle, so instead of handling sizing in create,
+        // it's clearer to do it here, and avoids doing it twice (create and resize are both called initially)
         camera.setToOrtho(false,width,height);
+        //set stage viewport
         stage.getViewport().update(width,height);
+        //get stage camera, so we can mess with it a bit
         OrthographicCamera stageCamera = (OrthographicCamera) stage.getCamera();
+        //zoom in the camera, and put it where it can see the main row
         stageCamera.zoom=.7f;
         stageCamera.setToOrtho(false,width,height);
     }
 
     @Override
 	public void render () {
+        //Wipe the screen clean with a white clear color
 		Gdx.gl.glClearColor(1,1,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //update the camera. just needs to happen each frame in case camera got moved
         camera.update();
+
+        //Batch and Shapes need the camera to transform their coordinates accordingly.
+        // ie. drawing at (x,y) can't just happen directly at those pixels, batch/shapes need to
+        // transform (x,y) based on the camera, so that they draw things to appear as if from the perspective of camera
         batch.setProjectionMatrix(camera.combined);
         shapes.setProjectionMatrix(camera.combined);
 
-        //Scene2d
+        //Scene2d. Step forward the world and draw the scene
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
         stage.setDebugAll(true);
 
+
+        //Draw the grid lines.
         shapes.begin(ShapeRenderer.ShapeType.Line);
         shapes.setColor(.88f, .88f, .88f, 1);
         //Vertical grid lines
@@ -120,6 +138,9 @@ public class Main extends ApplicationAdapter {
 	}
 
     public void dispose () {
+        //When the app is destroyed, don't leave any memory leaks behind
+        batch.dispose();
+        shapes.dispose();
         stage.dispose();
         skin.dispose();
     }
