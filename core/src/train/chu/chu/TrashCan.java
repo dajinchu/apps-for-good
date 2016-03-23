a560e5621bfa18582fee41106e9c51636060e920
Da-Jin Chu <dajinchu@gmail.com>
2016-03-23 10:08:46 -0400

    Re-added trashcan, fixing Arun's messed up merge cherry-picked commit c2b9
    Conflicts:
    	core/src/train/chu/chu/Main.java

--- core/src/train/chu/chu/TrashCan.java
+++ core/src/train/chu/chu/TrashCan.java
@@ -8,17 +8,20 @@ import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
  */
 public class TrashCan extends Image {
     public TrashCan(DragAndDrop dad){
+        //Trashcan Actor is really simple. It's just an Image that registers itself with drag and drop
         dad.addTarget(new DragAndDrop.Target(this){
             @Override
             public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                //Return true to indicate that the payload can be dropped on this can.
+                //Some things, like drag and drop keypad blocks should not be dropped on here, but
+                // it makes no difference either way
                 return true;
             }
 
             @Override
             public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
-                System.out.println("dropped");
                 if(source.getActor().getClass()==Block.class){
-                    System.out.println("remove");
+                    //Make SURE the source is a block before deleting it, don't want to be deleting keypad buttons
                     source.getActor().remove();
                 }
             }
