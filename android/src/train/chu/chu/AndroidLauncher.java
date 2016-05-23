478cad5c2d962974cdd26012bc9094a847eb3800
Da-Jin Chu <dajinchu@gmail.com>
2016-05-22 21:48:10 -0400

    Added Firebase analytics

--- android/src/train/chu/chu/AndroidLauncher.java
+++ android/src/train/chu/chu/AndroidLauncher.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import train.chu.chu.Main;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		FirebaseAnalytics.getInstance(this);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Main(), config);
 	}
